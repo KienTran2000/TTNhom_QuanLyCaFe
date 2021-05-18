284738dad217ae5c8489e4b596ffebdc5c65ebfd
KienTran2000 <77290046+KienTran2000@users.noreply.github.com>
2021-05-18 22:11:46 +0700

    fix

--- QuanLyCaFeLan1/QuanLyCaFeLan1/fLogin.cs
+++ QuanLyCaFeLan1/QuanLyCaFeLan1/fLogin.cs
@@ -18,40 +18,7 @@ namespace QuanLyCaFeLan1
             InitializeComponent();
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
-            string userName = txbUserName.Text;
-            string passWord = txbPassWord.Text;
-            if (Login(userName,passWord))
-            {
-                Account loginAccount = AccountDAO.Instance.GetAccountByUserName(userName);
-                fTableManager f = new fTableManager(loginAccount);
-                this.Hide();
-                f.ShowDialog();//chi thao tac tren dialog, chi xu ly tren manager,khi no tat di thi login moi hien len
-                this.Show();
-            }
-            else
-            {
-                MessageBox.Show("Sai tên tài khoản hoặc mật khẩu !");
-            }
-        }
-        bool Login(string userName,string passWord)
-        {
-            return AccountDAO.Instance.Login(userName, passWord);
-        }
 
-        private void btnExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if(MessageBox.Show("Bạn có thực sự muốn thoát","Thông báo",MessageBoxButtons.OKCancel,MessageBoxIcon.Warning) != System.Windows.Forms.DialogResult.OK)
-            {
-                e.Cancel = true;
-            }
-        }
 
         private void label1_Click(object sender, EventArgs e)
         {
